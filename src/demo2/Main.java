a926e8963ab5fa60d7e848362914f4532f5e4e15
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-06-04 11:41:42 +0700

    demo query database

---
+++ src/demo2/Main.java
@@ -0,0 +1,33 @@
+package demo2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args){
+        try{
+            // ket noi db
+            String connectionString = "jdbc:mysql://localhost:3306/t2409m_jp";
+            String username = "root";
+            String password = "root"; // dùng xampp: ""
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(connectionString,username,password);
+            System.out.println("Connected Database!");
+            Statement stt = conn.createStatement();
+            String sql = "select * from students";
+            ResultSet rs = stt.executeQuery(sql);
+            while (rs.next()){
+                String name = rs.getString("name");
+                String telephone = rs.getString("telephone");
+                Integer age = rs.getInt("age");
+                System.out.println(name);
+                System.out.println(telephone);
+                System.out.println(age);
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
