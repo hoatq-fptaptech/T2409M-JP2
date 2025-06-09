2ec9c368e8c1db5c3fab541d5fd2af0b87754ffc
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-06-09 09:35:43 +0700

    demo sql with object

--- src/demo2/Main.java
+++ src/demo2/Main.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
@@ -16,21 +17,28 @@ public class Main {
             Connection conn = DriverManager.getConnection(connectionString,username,password);
             System.out.println("Connected Database!");
             Statement stt = conn.createStatement();
-            // GET DATA
-//            String sql = "select * from students";
-//            ResultSet rs = stt.executeQuery(sql);
-//            while (rs.next()){
-//                String name = rs.getString("name");
-//                String telephone = rs.getString("telephone");
-//                Integer age = rs.getInt("age");
-//                System.out.println(name);
-//                System.out.println(telephone);
-//                System.out.println(age);
-//            }
+//             GET DATA
+            String sql = "select * from students";
+            ResultSet rs = stt.executeQuery(sql);
+
+            ArrayList<Student> listStudent = new ArrayList<>();
+
+            while (rs.next()){
+                Integer id = rs.getInt("id");
+                String name = rs.getString("name");
+                String telephone = rs.getString("telephone");
+                Integer age = rs.getInt("age");
+                Student s = new Student(id,name,age,telephone);
+                listStudent.add(s);
+            }
+
+            for (Student s : listStudent){
+                s.showInfo();
+            }
 
             // INSERT DATA
-            String insert_sql = "insert into students(name,age,telephone) values('Nguyễn Huy Long',19,'0988888888')";
-            stt.executeUpdate(insert_sql);
+//            String insert_sql = "insert into students(name,age,telephone) values('Nguyễn Huy Long',19,'0988888888')";
+//            stt.executeUpdate(insert_sql);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
---
+++ src/demo2/ShowStudent.java
@@ -0,0 +1,7 @@
+package demo2;
+
+public class ShowStudent {
+    public static void main(String[] args){
+        // viết code nhập vào id của 1 người và tìm trong db dữ liệu sinh viên đó
+    }
+}
